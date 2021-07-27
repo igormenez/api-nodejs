b296e78e7347b2990eb3af22efed7647cb43901f
Igor Menezes <50002202+igormenez@users.noreply.github.com>
2021-07-27 11:16:30 -0300

    Inital commit.

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: 'localhost',
+  username: 'nodeapi',
+  password: '1234',
+  database: 'nodeauth',
+  dialect: 'postgres',
+  operatorsAliasses: false,
+  logging: false,
+  define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true
+  }
+}; 
