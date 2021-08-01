d3acb587a85bb254d749cfb0cd3de5953bd7f765
Igor Menezes <50002202+igormenez@users.noreply.github.com>
2021-08-01 19:02:05 -0300

    Session test implemented.

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const { User } = require('../../src/app/models');
+
+const app = require('../../src/app');
+
+const  truncate  = require('../utils/truncate')
+
+describe('Authentication', ()=>{
+    beforeEach(async () => {
+        await truncate();
+    });
+    it('Should authenticate with valid credentials', async() => {
+        const user = await User.create({
+            name: 'Igor',
+            email: 'igor@igor.com',
+            password_hash: '12345' 
+        });
+
+        const response = await request(app)
+        .post('/sessions')
+        .send({
+            email: user.email,
+            password: '12345'
+        })
+        expect(response.status).toBe(200);
+    });
+})
--- src/database/migrations/20210727140313-create-users.js
+++ src/database/migrations/20210727140313-create-users.js
@@ -2,20 +2,38 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    return queryInterface.createTable('users',{
+      id:{
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allownull: false
+      },
+      name:{
+        type: Sequelize.STRING,
+        allownull: false
+      },
+      email:{
+        type: Sequelize.STRING,
+        unique: true,
+        allownull: false
+      },
+      password_hash:{
+        type: Sequelize.STRING,
+        allownull:false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    return queryInterface.dropTable('users');
   }
 };
